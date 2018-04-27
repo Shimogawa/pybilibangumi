286aa150504195e9658719609add2beaf051e61f
Rebuild <714026292@qq.com>
2018-04-26 20:17:34 -0400

    small modification

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ import bdatacollector.pybilirating
 
 
 if __name__ == '__main__':
-	csv_file = open('bangumi2.csv', 'a', newline='', encoding='utf-8')
-	brating = bdatacollector.pybilirating.PyBiliRating('bangumi2.csv', 'a')
-	brating.continueOnCsv()
+	brating = bdatacollector.pybilirating.PyBiliRating('bangumi.csv', 'w')
+	brating.startOver()
+	brating.close()
